13d5e568f3a315e57b67b28cf3a3d69f55e189a6
utko <guilherme@matilhaestud.io>
2023-05-21 17:08:39 -0300

    first

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+var serviceAccount = require("./keys/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
+app.get('/', (req, res) => {
+  db.collection('books').get()
+    .then((snapshot) => {
+      let books = [];
+      snapshot.forEach((doc) => {
+        let book = doc.data();
+        book.id = doc.id;
+        books.push(book);
+      });
+      if (books.length === 0) {
+        res.render('add-books.ejs');
+      } else {
+        res.render('index.ejs', { books: books });
+      }
+    })
+    .catch((error) => {
+      res.send(error.message);
+    });
+});
+
+app.post('/books', (req, res) => {
+  db.collection('books').add(req.body)
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((error) => {
+      res.send(error.message);
+    });
+});
+
+app.listen(5000, () => console.log('App listening on port 5000'));
